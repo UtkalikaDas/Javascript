f0eda79a7232f862f6734c974afebea054000215
Utkalika_ Das <utkalikadas39@gmail.com>
2025-04-05 07:02:03 +0000

    Constant & Variables

---
+++ 1.constant.js
@@ -0,0 +1,32 @@
+//constant -> Which cannot change the value and the variables.
+//   "const" is Constant.
+
+//case 1
+const abc = 24
+const abc = 21
+
+
+//case 2
+const user1 = "utkalika"
+const user2 = "utkalika"
+
+// variables -> 
+// variables are of 2 types "var" and "let"
+
+// let keyword support block scope
+
+var name = "pagala"
+{var name = "paga"}
+console.log(name)
+
+// where as var keyword does not support block scope.
+
+let name = "pagala"
+{let name = "paga"}
+console.log(name)
+
+//{} -> This is called block scope
+
+
+
+
